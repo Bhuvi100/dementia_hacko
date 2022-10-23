5bbe9763d21810c001707f12340e009e9bc3df6e
ashwinbiju09 <20eucs018@skcet.ac.in>
2022-10-23 16:21:22 +0530

    Updates

--- frontend/src/components/CaretakerCard.js
+++ frontend/src/components/CaretakerCard.js
@@ -3,11 +3,11 @@ import React from 'react'
 const CaretakerCard = () => {
   return (
     <div>
-    <h1 class="mt-2 text-center font-bold py-5 text-2xl">Care Takers Status</h1>
-    <div class="flex font-medium items-center justify-center">
-    <div class="w-36 mx-auto bg-[#6943a4] rounded-2xl px-8 py-6 shadow-lg">
+    <h1 class="mt-2 text-center font-bold py-5 text-2xl">Care Takers Availability</h1>
+    <div class="flex font-medium items-center justify-center mx-2">
+    <div class="w-36 mx-2 bg-[#6943a4] rounded-2xl px-2 py-2 shadow-lg">
         <div class="mt-1 w-fit mx-auto">
-            <img src="/assets/user.png" class="rounded-full w-28 " />
+            <img src="/assets/user.png" class="rounded-full w-24" />
         </div>
 
         <div class="mt-1">
@@ -17,18 +17,31 @@ const CaretakerCard = () => {
             Active
         </p>
     </div>
-    <div class="w-36 mx-auto bg-[#6943a4] rounded-2xl px-8 py-6 shadow-lg">
+    <div class="w-36 mx-2 bg-[#6943a4] rounded-2xl px-2 py-2 shadow-lg">
         <div class="mt-1 w-fit mx-auto">
-            <img src="/assets/user.png" class="rounded-full w-28 " />
+            <img src="/assets/user.png" class="rounded-full w-24" />
         </div>
 
         <div class="mt-1">
-            <h2 class="text-white text-base text-center">Katy D'Or</h2>
+            <h2 class="text-white text-base text-center">Katy Dor</h2>
         </div>
         <p class="text-emerald-300 text-sm text-center" >
             Inactive
         </p>
     </div>
+    <div class="w-36 mx-2 bg-[#6943a4] rounded-2xl px-2 py-2 shadow-lg">
+        <div class="mt-1 w-fit mx-auto">
+            <img src="/assets/user.png" class="rounded-full w-24" />
+        </div>
+
+        <div class="mt-1">
+            <h2 class="text-white text-base text-center">Simon</h2>
+        </div>
+        <p class="text-emerald-300 text-sm text-center" >
+            Inactive
+        </p>
+    </div>
+    
     </div>
     </div>
   )
--- frontend/src/pages/GuardianDashboard.js
+++ frontend/src/pages/GuardianDashboard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import CaretakerCard from '../components/CaretakerCard'
 import GuardianNavbar from '../components/GuardianNavbar'
-import React from "react";
-import GuardianNavbar from "../components/Guardiannavbar";
 import Chart from "../components/Chart";
 
 const GuardianDashboard = () => {
   return (
     <div>
         <GuardianNavbar />
+        <h1 class="mt-2 text-center font-bold py-5 text-2xl">Analysis Report</h1>
+        <Chart />
         <CaretakerCard />
         <div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-10">
             <div class="p-5 bg-purple-200 ml-8 rounded-lg h-32">
@@ -78,14 +78,25 @@ const GuardianDashboard = () => {
         </button>
       </p>
       <h2 class="font-semibold text-lg text-center text-gray-800 mt-1">
-        Memory Test
+        Memory Test 
+      </h2>
+      </div>
+      
+      <div class="p-5 bg-purple-200 mr-8 rounded-lg h-32">
+      <p class="text-center">
+      <button>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-9 h-9">
+        <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+        </svg>
+      </button>
+      </p>
+      <h2 class="font-semibold text-lg text-center text-gray-800 mt-1">
+        User Activity
       </h2>
       </div>
 
     </div>
     <br />
-      <GuardianNavbar />
-      <Chart />
     </div>
   );
 };
